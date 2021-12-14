95ccfb9c0ed6f40310e3b8a561ecaf59dfe06da2
Emmanuell87 <emmanuelmejia2003@gmail.com>
2021-12-14 17:02:06 -0500

    arrow, manejo de errores

---
+++ README.md
@@ -0,0 +1,8 @@
+# cryptobot
+1. pip3 install discord.py
+2. pip3 install python-dotenv
+
+#.env config
+1. TOKEN = YOUR_TOKEN_BOT
+2. TOKEN_ID = YOUR_CONSULT_CRYPTO
+3. UPDATE_TIME = SECONDS
--- index.py
+++ index.py
@@ -12,7 +12,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 TOKEN_ID = os.environ.get("TOKEN_ID")
 TOKEN = os.environ.get("TOKEN")
-UPDATE_TIME = os.environ.get("UPDATE_TIME")
+UPDATE_TIME = int(os.environ.get("UPDATE_TIME"))
+DECIMALS = int(os.environ.get("DECIMALS"))
 
 def data_crypto():#consultamos los datos de la cryptomoneda
     response = request.urlopen('https://api.coingecko.com/api/v3/coins/' + TOKEN_ID).read()
@@ -28,43 +29,52 @@ bot = commands.Bot(command_prefix='!', description="This is a helper bot")
 # Events
 @bot.event
 async def on_ready():
-    print('My Bot is ready')
+        print('My Bot is ready')
     
-    #actualizamos el avatar del bot una sola vez
-    req = request.Request(data_crypto()['image']['large'], headers={'User-Agent': 'Mozilla/5.0'})
-    img = request.urlopen(req).read()
-    await bot.user.edit( avatar=img)
+        #actualizamos el avatar del bot una sola vez
+        try:
+            req = request.Request(data_crypto()['image']['large'], headers={'User-Agent': 'Mozilla/5.0'})
+            img = request.urlopen(req).read()
+            await bot.user.edit( avatar=img)
+        finally:
+            while not bot.is_closed():
+                try:
+                    crypto = data_crypto()#obtenemos los datos de la consulta
 
-    while not bot.is_closed():
-        crypto = data_crypto()#obtenemos los datos de la consulta
+                    for guild in bot.guilds: #obtenemos los datos de la "guild" para cambiar el role y el apodo(nickname)
+                        
+                        #obtenemos los roles para cambiar el color del apodo
+                        role_low = get(guild.roles, name='low')
+                        role_high = get(guild.roles, name='high')
 
-        for guild in bot.guilds: #obtenemos los datos de la "guild" para cambiar el apodo y el role
-            await guild.me.edit(nick=crypto['symbol'].upper() + ' ' + str(crypto['market_data']['current_price']['usd']), color=discord.Color.green())
-            
-            #obtenemos los roles para cambiar el color del apodo
-            role_low = get(guild.roles, name='low')
-            role_high = get(guild.roles, name='high')
+                        #if los roles no existe los crea
+                        if(role_low == None):
+                            role_low = await guild.create_role(name="low", permissions=discord.Permissions(), colour=discord.Colour.red())
+                        if(role_high == None):
+                            role_high = await guild.create_role(name="high", permissions=discord.Permissions(), colour=discord.Colour.green())
+                        
+                        # almacenara la flecha que llevara el nombre        
+                        type_arrow = '' 
+                        #if el porcentaje bajo el role asignado será "low" de lo contrario será "high", con sus recpectiva flecha
+                        if(crypto['market_data']['price_change_percentage_24h'] < 0):
+                            await guild.me.add_roles(role_low)
+                            await guild.me.remove_roles(role_high)
+                            type_arrow = '↘️'
+                        else:
+                            await guild.me.add_roles(role_high)
+                            await guild.me.remove_roles(role_low)
+                            type_arrow = '↗️'
 
-            #if los roles no existe los crea
-            if(role_low == None):
-                role_low = await guild.create_role(name="low", permissions=discord.Permissions(268435456), colour=discord.Colour.red())
-            if(role_high == None):
-                role_high = await guild.create_role(name="high", permissions=discord.Permissions(268435456), colour=discord.Colour.green())
+                        await guild.me.edit(nick=crypto['symbol'].upper() + f' {type_arrow} ' + str(crypto['market_data']['current_price']['usd']), color=discord.Color.green())
+                    #cambia la "presence" asignandolo un nombre del cambio del precio en 24h y el cambio del pocentaje en 24h
+                    await bot.change_presence(activity=discord.Game(
+                        name = str(round(crypto['market_data']['price_change_24h'], DECIMALS)) +  
+                        f"({str(round(crypto['market_data']['price_change_percentage_24h'], DECIMALS))}%)"
+                    ))
 
-            #if el porcentaje bajo el role asignado será "low" de lo contrario será "high"
-            if(crypto['market_data']['price_change_percentage_24h'] < 0):
-                await guild.me.add_roles(role_low)
-                await guild.me.remove_roles(role_high)
-            else:
-                await guild.me.add_roles(role_high)
-                await guild.me.remove_roles(role_low)
-        #cambia la "presence" asignandolo un nombre del cambio del precio en 24h y el cambio del pocentaje en 24h
-        await bot.change_presence(activity=discord.Game(
-            name = str(round(crypto['market_data']['price_change_24h'], 2)) +  
-            f"({str(round(crypto['market_data']['price_change_percentage_24h'], 2))}%)"
-        ))
-
-        #tiempo de espera para volver a actualizar
-        await asyncio.sleep(int(UPDATE_TIME))
-    
+                    #tiempo de espera para volver a actualizar
+                except BaseException as err:
+                    print(err)
+                    
+                await asyncio.sleep(UPDATE_TIME)
 bot.run(TOKEN)
